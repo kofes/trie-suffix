5c54c165fc46f21d67f2541660c1ebb8b76b5385
kofes <turnever@gmail.com>
2017-05-26 22:29:28 +1000

    TS update

--- inc/Trie_suffix.hpp
+++ inc/Trie_suffix.hpp
@@ -15,6 +15,7 @@ public:
     void clear();
     bool substring_exist(const std::string& key);
     void print(std::ofstream& f_out);
+    void print();
 private:
     struct Node {
       Node (size_t _left, size_t *_right) : left(_left), right(_right) {
@@ -32,10 +33,16 @@ private:
     };
 
     void print_node (Node* node, size_t deep, char sym, std::ofstream& f_out);
+    void print_node (Node* node, size_t deep, char sym);
 
     Node* find_prefix_suffix(size_t ind, bool& is_fork);
 
     std::vector<Node*> child;
     std::string text;
+    Node* last_passed;
+    size_t last_pos_in_passed;
     size_t last_sym_pos, from_pos;
+
+    Trie_suffix::Node* prev;
+    std::vector<Node*>* prev_child;
 };
--- src/Trie_suffix.cpp
+++ src/Trie_suffix.cpp
@@ -5,6 +5,8 @@ Trie_suffix::Trie_suffix() {
     text.clear();
     last_sym_pos = 0;
     from_pos = 0;
+    last_passed = nullptr;
+    last_pos_in_passed = 0;
 }
 
 Trie_suffix::~Trie_suffix() {
@@ -20,20 +22,43 @@ void Trie_suffix::clear() {
   text.clear();
   last_sym_pos = 0;
   from_pos = 0;
+  last_passed = nullptr;
+  last_pos_in_passed = 0;
 }
 
 Trie_suffix::Node* Trie_suffix::find_prefix_suffix(size_t ind, bool& is_fork) {
-  size_t curr_ind;
-  Trie_suffix::Node* curr = child[text[ind]], *prev = nullptr;
-  std::vector<Node*>* prev_child = &child;
+  size_t curr_ind = 0;
+  Trie_suffix::Node* curr = child[text[ind]];
+  //Optimization
+  if (last_passed != nullptr) {
+    curr = last_passed;
+    curr_ind = last_pos_in_passed;
+    ind = last_sym_pos - curr_ind;
+    // //DEBUG
+    // std::cout << "CURR-SUFF: ";
+    // for (size_t i = curr->left; i < text.length(); ++i)
+    //   std::cout << text[i];
+    // std::cout << std::endl;
+    // std::cout << "CURR-TEXT: ";
+    // for (size_t i = curr->left; i < *(curr->right)+((curr->is_tmp)? 0 : 1); ++i)
+    //   std::cout << text[i];
+    // std::cout << std::endl;
+    // std::cout << "DIFF: " << text[curr->left + curr_ind] << ':' << text[ind + curr_ind] << std::endl;
+    // //
+  } else {
+    prev = nullptr;
+    prev_child = &child;
+  }
+  //
   while (curr != nullptr) {
-    for (curr_ind = 0; curr_ind < *(curr->right)-curr->left+((curr->is_tmp)? 0 : 1) && curr_ind < text.length() - ind; ++curr_ind)
+    for (; curr_ind < *(curr->right)-curr->left+((curr->is_tmp)? 0 : 1) && curr_ind < text.length() - ind; ++curr_ind)
       //Check fork
       if (text[curr->left + curr_ind] != text[ind+curr_ind]) {
         Trie_suffix::Node* tmp = new Trie_suffix::Node(curr->left, &(curr->left));
         tmp->is_tmp = true;
         curr->left = curr->left + curr_ind;
         (*prev_child)[text[tmp->left]] = tmp;
+        // std::cout << "TMP-LEFT: " << text[tmp->left] << std::endl;
         tmp->child[text[curr->left]] = curr;
         if (prev != nullptr && *(prev->right) > tmp->left) prev->right = &(tmp->left);
 
@@ -41,26 +66,73 @@ Trie_suffix::Node* Trie_suffix::find_prefix_suffix(size_t ind, bool& is_fork) {
 
         tmp->child[text[ind]] = new Trie_suffix::Node(ind, &last_sym_pos);
         ++from_pos;
+        // //DEBUG
+        // std::cout << "FROM-POS>FORK: " << from_pos << std::endl;
+        // if (from_pos == 13) {
+        //   std::cout << "-------------" << std::endl;
+        //   print();
+        //   std::cout << "-------------" << std::endl;
+        // }
+        // //
         is_fork = true;
+        // //DEBUG
+        // std::cout << "CURR-IND: " << curr_ind << std::endl;
+        // //
+        //Optimization
+        last_passed = nullptr;
+        last_pos_in_passed = 0;
+        // //DEBUG
+        // std::cout << "LAST-POS>FORK: " << last_pos_in_passed << std::endl;
+        // //
+        //
         return tmp->child[text[ind]];
       }
     ind+=curr_ind;
     //It's prefix
     if (ind == text.length()) {
       is_fork = false;
+      //Optimization
+      last_passed = curr;
+      last_pos_in_passed = curr_ind;
+      // //DEBUG
+      // std::cout << "CURR-TEXT: ";
+      // for (size_t i = curr->left; i < text.length(); ++i)
+      //   std::cout << text[i];
+      // std::cout << std::endl;
+      // std::cout << "CUR-POS>PREF: " << last_pos_in_passed << std::endl;
+      // std::cout << "LAST-IND>PREF: " << ind << std::endl;
+      // //
+      //
       return curr;
     }
     //It's new tail from node -> fork
     if (curr->is_tmp && curr->child[text[ind]] == nullptr) {
       curr->child[text[ind]] = new Trie_suffix::Node(ind, &last_sym_pos);
       ++from_pos;
+      // //DEBUG
+      // std::cout << "FROM-POS>TAIL: " << from_pos << std::endl;
+      // //
       is_fork = true;
+      //Optimization
+      last_passed = nullptr;
+      last_pos_in_passed = 0;
+      // //DEBUG
+      // std::cout << "LAST-POS>TAIL: " << last_pos_in_passed << std::endl;
+      // //
+      //
       return curr->child[text[ind]];
     }
 
     prev_child = &curr->child;
     prev = curr;
     curr = curr->child[text[ind]];
+    curr_ind = 0;
+    //Optimization
+    last_passed = nullptr;
+    last_pos_in_passed = 0;
+    // //DEBUG
+    // std::cout << "LAST-POS>BREAK: " << last_pos_in_passed << std::endl;
+    // //
   }
 
   return nullptr;
@@ -102,6 +174,16 @@ void Trie_suffix::add_suffix(char sym) {
     if (curr == nullptr) {//suffix is not found => add sym into root->childs
       child[sym] = new Trie_suffix::Node(text.length()-1, &last_sym_pos);
       from_pos = text.length();
+      // //DEBUG
+      // std::cout << "FROM-POS>NEW: " << from_pos << std::endl;
+      // //
+      //Optimization
+      last_passed = nullptr;
+      last_pos_in_passed = 0;
+      // //DEBUG
+      // std::cout << "LAST-POS>NEW: " << last_pos_in_passed << std::endl;
+      // //
+      //
       break;
     }
     if (!is_fork)//suffix is prefix => do nothing
@@ -120,6 +202,16 @@ void Trie_suffix::print(std::ofstream& f_out) {
       print_node(child[i], 1, i, f_out);
 }
 
+void Trie_suffix::print() {
+  for (size_t i = 0; i < child.size()/2; ++i)
+      print_node(child[i], 1, i);
+
+  std::cout << '.' << std::endl;
+
+  for (size_t i = child.size()/2; i < child.size(); ++i)
+      print_node(child[i], 1, i);
+}
+
 void Trie_suffix::print_node (Node* node, size_t deep, char sym, std::ofstream& f_out) {
   if (node == nullptr) return;
 
@@ -133,4 +225,19 @@ void Trie_suffix::print_node (Node* node, size_t deep, char sym, std::ofstream& 
 
   for (size_t i = node->child.size()/2; i < child.size(); ++i)
     print_node(node->child[i], deep+1, i, f_out);
+}
+
+void Trie_suffix::print_node (Node* node, size_t deep, char sym) {
+  if (node == nullptr) return;
+
+  for (size_t i = 0; i < node->child.size()/2; ++i)
+    print_node(node->child[i], deep+1, i);
+
+  std::cout << std::string(deep*8, ' ');
+  for (size_t i = node->left; i < *(node->right)+((node->is_tmp)? 0 : 1); ++i)
+    std::cout << text[i];
+  std::cout << std::endl;
+
+  for (size_t i = node->child.size()/2; i < child.size(); ++i)
+    print_node(node->child[i], deep+1, i);
 }
